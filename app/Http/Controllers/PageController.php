fced4d0e48d84a332611ca68d476c842a30d8073
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-06 22:01:00 -0500

    Estrcutura para los post

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function posts () {
+        return view('post', [
+            "posts" => Post::load("user")->latest()->paginate(),
+        ]);
+    }
+    public function post (Post $post) {
+        return view("post", ["post" => $post ]);
+    }
 }
